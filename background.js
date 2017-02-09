cc4d48a16a777c6b789a0c4cf7234d03e8231105
Ivan Gadzhega <ivgadzhega@gmail.com>
2017-02-09 17:39:53 +0200

    Revrite XHR requests with promises and simple error handling

--- background.js
+++ background.js
@@ -22,54 +22,93 @@ function getCurrentRevenueUrl(id, period) {
 // HTTP Requests
 // --------------------------------------------------
 
+function get(url) {
+    return new Promise(function(resolve, reject) {
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState !== 4) return;
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+                showErrorBadge();
+            }
+        };
+
+        xhr.open('GET', url, true);
+        xhr.send();
+    });
+}
+
 function getPublisherId() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", getPublisherOverviewUrl(), true);
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
-		var resp = JSON.parse(xhr.responseText);
-		getCurrentPeriod(resp.overview.id)
-	  }
-	};
-	xhr.send();
+    var id = get(getPublisherOverviewUrl())
+        .then(JSON.parse)
+        .then(function (result) {
+            return result.overview.id;
+        });
+
+    return id;
 }
 
 function getCurrentPeriod(id) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", getCurrentPeriodUrl(id), true);
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
-		var resp = JSON.parse(xhr.responseText);
-		getCurrentRevenue(id, resp.periods[0].value);
-	  }
-	};
-	xhr.send();
+
+    var period = get(getCurrentPeriodUrl(id))
+        .then(JSON.parse)
+        .then(function (result) {
+            return result.periods[0].value;
+        });
+
+    return period;
 }
 
-function getCurrentRevenue(id, period) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", getCurrentRevenueUrl(id, period), true);
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
-		var resp = JSON.parse(xhr.responseText);
-		var arr = resp.aaData;
-		var total= 0.0;
-		for(var i in arr) {
-			total += parseFloat(arr[i][5].replace(/\$|,/g, ''));
-		}
-		
-		total *= 0.7;
-		chrome.browserAction.setBadgeText({ text: total.toString() + "$" } );
-	  }
-	};
-	xhr.send();
+function getCurrentRevenue() {
+    getPublisherId().then(function(id) {
+        getCurrentPeriod(id).then(function(period) {
+            get(getCurrentRevenueUrl(id, period))
+                .then(JSON.parse)
+                .then(function (result) {
+                        var arr = result.aaData;
+                        var total= 0.0;
+                        for(var i in arr) {
+                            total += parseFloat(arr[i][5].replace(/\$|,/g, ''));
+                        }
+                        total = Math.round(total * 0.7);
+                        showRevenueBadge(total);
+                }, chainError);
+        }, chainError);
+    }, chainError);
 }
+
+function chainError(err) {
+    showErrorBadge();
+    return Promise.reject(err);
+};
 
 // --------------------------------------------------
 // Actions
 // --------------------------------------------------
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.browserAction.setBadgeText({ text: "..." } );
-  getPublisherId();
+	showLoadingBadge();
+    getCurrentRevenue();
 });
+
+// --------------------------------------------------
+// Visual
+// --------------------------------------------------
+
+function showLoadingBadge() {
+    chrome.browserAction.setBadgeBackgroundColor({color:[125,125,125,255]});
+    chrome.browserAction.setBadgeText({ text: ". . ." } );
+}
+
+function showRevenueBadge(revenue) {
+    chrome.browserAction.setBadgeBackgroundColor({color:[0,125,100,255]});
+    chrome.browserAction.setBadgeText({ text: revenue.toString() + "$" } );
+}
+
+function showErrorBadge() {
+	chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,255]});
+    chrome.browserAction.setBadgeText({ text: "ERR" } );
+}
